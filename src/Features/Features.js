f445015e6aeac897e2574a2fceb81a0fd7ae70f5
prae-torius <arnettepreston@gmail.com>
2019-04-06 21:21:54 -0400

    cleanup Features and Options

--- src/Features/Features.js
+++ src/Features/Features.js
@@ -3,22 +3,22 @@ import Options from '../Options/Options';
 
 class Features extends React.Component {
   render() {
-      const feature = Object.keys(this.props.features)
-      .map(key => { 
-        return (
+    const feature = Object.keys(this.props.features)
+    .map(key => { 
+      return (
         <div className="feature" key={key}>
-        <div className="feature__name">{key}</div>
-        <ul className="feature__list">
-        <Options 
-          features={this.props.features[key]} 
-          selected={this.props.selected[key]} 
-          updateFeature={this.props.updateFeature}
-          optionKey={key}
-          /> 
-        </ul>
-      </div>
-          )
-        })
+          <div className="feature__name">{key}</div>
+          <ul className="feature__list">
+            <Options 
+              features={this.props.features[key]} 
+              selected={this.props.selected[key]} 
+              updateFeature={this.props.updateFeature}
+              optionKey={key}
+            /> 
+           </ul>
+        </div>
+      )
+    })
   
     return (
       <div className='Features'>
@@ -27,4 +27,5 @@ class Features extends React.Component {
     )
   } 
 }
+
 export default Features;
